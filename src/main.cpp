a58d4609138850b66b7147a4b1af6553a3655a9f
Jayson030 <Zeyulei6@gmail.com>
2026-04-01 02:15:40 +0100

    Fix: make save_config path consistent with load_config

--- src/main.cpp
+++ src/main.cpp
@@ -93,7 +93,7 @@ int main(int argc, char* argv[]) {
     }
 
     global_app = nullptr;
-    Scalpel::Config::save_config();
+    Scalpel::Config::save_config("config/config.txt");
     std::println("[System] Application cleanly exited. Kernel resources fully released.");
 
     return ret;
